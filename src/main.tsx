a1b23fdeac47619f3d423ea57c87ecc54017dc69
Guillaume Guerin <guillaume.guerin@epitech.eu>
2023-01-18 10:37:24 +0100

    Use Route instead of BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Root from "./routes/Root";
 import Characters from "./views/Characters";
@@ -12,32 +18,42 @@ import Episodes from "./views/Episodes";
 
 const queryClient = new QueryClient();
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <h1>404</h1>,
-    children: [
-      {
-        path: "/characters",
-        element: <Characters />,
-      },
-      {
-        path: "/episodes",
-        element: <Episodes />,
-      },
-    ],
-  },
-  {
-    path: "/character/:id",
-    element: <Character />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     errorElement: <h1>404</h1>,
+//     children: [
+//       {
+//         path: "/characters",
+//         element: <Characters />,
+//       },
+//       {
+//         path: "/episodes",
+//         element: <Episodes />,
+//       },
+//     ],
+//   },
+//   {
+//     path: "/character/:id",
+//     element: <Character />,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Root />}>
+            <Route index path="characters" element={<Characters />} />
+            <Route path="episodes" element={<Episodes />} />
+            <Route path="character/:id" element={<Character />} />
+            <Route path="*" element={<Navigate replace to="/characters" />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
